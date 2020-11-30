9c098a36f86bc520f75cf6b5bfd9b16aa679e456
Ralph <admin@ralphbrando.com>
2020-11-30 15:14:46 +0100

    f

--- odoomoduleyes/__manifest__.py
+++ odoomoduleyes/__manifest__.py
@@ -10,7 +10,7 @@
         Long description of module's purpose
     """,
 
-    'author': "Abdessamad",
+    'author': "Yeesss",
     'website': "http://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
